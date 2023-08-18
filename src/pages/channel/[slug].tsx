f9b2ff08a8662782af38dd7401081ef428ecb694
Nicolas Klein <myxoh10@gmail.com>
2023-08-18 15:28:34 +0100

    Removes more irrelevant things - makes issue cleearer

--- src/pages/channel/[slug].tsx
+++ src/pages/channel/[slug].tsx
@@ -7,11 +7,11 @@ function Slug() {
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <span>Hello {slug}</span>
-      <Link href="/channel/test-1">Channel 1</Link>
-      <Link href="/channel/test-2">Channel 2</Link>
-      <Link href="/channel/test-3">Channel 3</Link>
-      <Link href="/channel/test-4">Channel 4</Link>
-      <Link href="/channel/test-20">Channel 20</Link>
+      <Link href="/channel/test-1">This does not reload (1)</Link>
+      <Link href="/channel/test-2">This reloads (2)</Link>
+      <Link href="/channel/test-3">This does not reload (3)</Link>
+      <Link href="/channel/test-4">This does not reload (4)</Link>
+      <Link href="/channel/test-20">This does not reload (20)</Link>
     </div>
   );
 }
